8b54f6819f9a15ec3d4727195caddc10e1e650f4
hyeonsang <k98hh@naver.com>
2026-03-06 11:08:45 +0900

    엑스포 앱 실행설정 수정

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -6,4 +6,11 @@ const config = getDefaultConfig(__dirname);
 // Watch shared types/tools from monorepo root
 config.watchFolders = [path.resolve(__dirname, '../src/shared')];
 
+// Allow Metro to resolve .mjs files (required for @google/genai)
+config.resolver.sourceExts = [...config.resolver.sourceExts, 'mjs', 'cjs'];
+config.resolver.assetExts = config.resolver.assetExts.filter(ext => ext !== 'mjs');
+
+// Resolve node_modules from mobile directory even for files outside (e.g. ../src/shared/*)
+config.resolver.nodeModulesPaths = [path.resolve(__dirname, 'node_modules')];
+
 module.exports = config;
